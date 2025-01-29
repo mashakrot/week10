a17ad9f66c26a299052ae4dd1b7e356ca7d377a2
mashakrot <makrosensei@gmail.com>
2025-01-29 14:00:37 +0200

    last last

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// import tseslint from 'typescript-eslint'
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -12,13 +15,26 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsparser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      react: { version: "18.3" },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react,
+      "@typescript-eslint": tseslint, 
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...tseslint.configs.recommendedTypeChecked.rules,
+      ...tseslint.configs.stylisticTypeChecked.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
